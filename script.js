f34876aed40202c7245b9fc10ccdf102655d3439
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-17 15:39:53 +0200

    fixing input

--- script.js
+++ script.js
@@ -3,12 +3,45 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            newListElement: '',
             todoList: [
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'Pagare le bollette',
+                    done: false
+                },
+                {
+                    text: 'Andare al mare',
+                    done: false
+                },
+                {
+                    text: 'Pulire la macchina',
+                    done: false
+                },
+            ]
+        }
+    },
+
+    methods: {
+        removeElement(index) {
+            this.todoList.splice(index, 1);
+        },
+
+        addNewListElement(content) {
+            this.todoList.push(
                 {
                     text: '',
                     done: false
                 }
-            ]
+            );
+            this.newListElement = "";
+        },
+
+        isDone(index) {
+            this.todoList[index].done = !this.todoList[index].done;
         }
     }
 
